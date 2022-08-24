19b51c54f66537a0cf83399bc7a206c24c6881fb
Yoon Hwa Jeong <yjeong0114@hotmail.com>
2022-08-24 10:24:22 -0500

    first commit

---
+++ module.py
@@ -0,0 +1,10 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+def squareFootage(len,wid):
+    area = len * wid
+    print(f"The square footage of a house is {area}")
+
+def circum(diam):
+    cir = diam * 3.14
+    print(f"The circumference of a circle is {cir}")
